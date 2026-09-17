0b82b2c1312d5a0e857aadf5138eeb73dc7f0568
Ld4vicl <connguathanhtroia@gmail.com>
2026-09-17 11:36:38 +0700

    done parser cmd

--- main.c
+++ main.c
@@ -1,42 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 struct command
 {
-    char command_name[10];
-    char *argv[10];
+    char *argv[10]; // toi da 1 cau lenh co 9 tham so
+    char argc;
+    // void cmd = argv[0]; // phan tu dau tien
 } command;
-
+struct command command;
+void strip(char *cmd)
+{
+    int start_idx = 0;
+    int len_cmd = strlen(cmd) - 1; // just idx
+    int end_idx = len_cmd;
+    while (cmd[start_idx] == ' ' || cmd[end_idx] == ' ')
+    {
+        if (cmd[start_idx] == ' ')
+            start_idx++;
+        if (cmd[end_idx] == ' ')
+            end_idx--;
+    }
+    char tmp_str[len_cmd];
+    memset(tmp_str, 0, len_cmd);
+    // printf("start-end : %d-%d\n", start_idx, end_idx);
+    strncpy(tmp_str, &cmd[start_idx], end_idx - start_idx + 1);
+    // printf("tmp : <%s>\n", tmp_str);
+    memset(cmd, 0, 0x100);
+    strncpy(cmd, tmp_str, strlen(tmp_str));
+}
 void parser_cmd(char *cmd)
 {
-    int max_len = strlen(cmd);
-    // we gonna split into command and argv
-    struct command command;
-
-    for (int i = 0; i < max_len; i++) // remove all whitespace at the begining
+    for (int i = 0; i < 10; i++)
     {
-        if (cmd[i] != ' ')
+        command.argv[i] = malloc(0x10);
+    }
+    // printf("cmd : <%s>\n", cmd);
+    strip(cmd);
+    // printf("cmd : <%s>\n", cmd);
+    int len_cmd = strlen(cmd);
+    int count = 0;
+    command.argc = 0;
+    char tmp[len_cmd];
+    memset(tmp, 0, len_cmd);
+
+    for (int i = 0; i < len_cmd; i++)
+    {
+
+        if (cmd[i] == ' ') // ' ls -lh -a '
         {
-            strcpy(cmd, &cmd[i]);
-            // printf("\'%s\'", cmd);
-            break;
+            strncpy(command.argv[command.argc], tmp, strlen(tmp));
+            command.argc++;
+            memset(tmp, 0, len_cmd);
+            continue;
+        }
+        else
+        {
+            if (i == len_cmd - 1)
+            {
+                strncpy(command.argv[command.argc], tmp, strlen(tmp));
+                command.argc++;
+                break;
+            }
+            char c = cmd[i];
+            strncat(tmp, &c, 1);
+            // printf("tmp : %s\n", tmp);
         }
     }
-    strncpy(command.command_name, cmd, strcspn(cmd, " "));
-    strcpy(cmd, &cmd[strcspn(cmd, " ") + 1]); // 'ls -lh -a ' -> '-lh -a '
-
-    int i, j, len = 0;
-    while (i <= max_len)
+    for (int i = 0; i < command.argc; i++)
     {
-        while (cmd[len] != ' ')
-        {
-            len += 1;
-        }
-        strncpy(command.argv[j], cmd, len);
-        i += len;
-        j += 1;
-        len += 1; // ignore next ' '
+        printf("aaa %s\n", command.argv[i]);
     }
 }
 
@@ -49,8 +83,9 @@ int main()
         memset(cmd, 0, 0x100);
 
         printf("\n$ ");
-        scanf("%s", cmd);
-        cmd[strlen(cmd)] = '\x00';
+        fflush(stdout);
+        read(0, cmd, 100);
+        cmd[strlen(cmd) - 1] = '\x00'; // ' ls -lh -a \n'
         parser_cmd(cmd);
     }
 }
--- test.c
+++ test.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
+void strip1(char *a)
+{
+    strcpy(a, "123");
+}
+
+void strip2(char **a)
+{
+    strcpy(*a, "123");
+}
 int main()
 {
-
-    char c[] = "      a ls -lha   ";
-    strcspn(c, " ");
-    printf("%s",c);
+    char a[10] = "abc";
+    strip1(a);
+    printf("a : %s\n", a);
+    char *b = a;
+    strip2(&b);
+    printf("a : %s\n", a);
 }
