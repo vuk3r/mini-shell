91273af4138919cd0f5ba899c8b1e16e59b03e6e
Ld4vicl <connguathanhtroia@gmail.com>
2026-09-17 16:02:37 +0700

    error echo 'a'a'

--- main.c
+++ main.c
@@ -9,7 +9,80 @@ struct command
     char argc;
     // void cmd = argv[0]; // phan tu dau tien
 } command;
+
+void strip(char *cmd);
+void parser_cmd(char *cmd);
+void ls();
+void pwd();
+void cd();
+void echo();
+void cat();
+
 struct command command;
+
+int main()
+{
+
+    while (1)
+    {
+        char cmd[0x100];
+        memset(cmd, 0, 0x100);
+
+        for (int i = 0; i < command.argc; i++)
+        {
+            memset(command.argv[i], 0, 10);
+        }
+        printf("\n$ ");
+        fflush(stdout);
+        read(0, cmd, 100);
+        cmd[strlen(cmd) - 1] = '\x00'; // ' ls -lh -a \n'
+        parser_cmd(cmd);
+        memset(cmd, 0, 0x100);
+
+        strcpy(cmd, command.argv[0]);
+        for (int i = 0; i < command.argc; i++)
+        {
+            printf(" %s\n", command.argv[i]);
+        }
+        if (!strcmp(cmd, "ls"))
+        {
+            ls();
+        }
+        else if (strcmp(cmd, "pwd"))
+        {
+            pwd();
+        }
+        else if (strcmp(cmd, "cd"))
+        {
+            cd();
+        }
+        else if (strcmp(cmd, "echo"))
+        {
+            echo();
+        }
+        else if (strcmp(cmd, "cat"))
+        {
+            cat();
+        }
+    }
+}
+void ls()
+{
+    printf("ls called !");
+}
+void cd()
+{
+}
+void pwd()
+{
+}
+void cat()
+{
+}
+void echo()
+{
+    // echo 'abc xyz'
+}
 void strip(char *cmd)
 {
     int start_idx = 0;
@@ -45,12 +118,14 @@ void parser_cmd(char *cmd)
     char tmp[len_cmd];
     memset(tmp, 0, len_cmd);
 
-    for (int i = 0; i < len_cmd; i++)
+    for (int i = 0; i < len_cmd; i++) // 'ls '
     {
 
-        if (cmd[i] == ' ') // ' ls -lh -a '
+        if (cmd[i] == ' ') // 'ls -lh -a'
         {
+            printf("tmp : %s\n", tmp);
             strncpy(command.argv[command.argc], tmp, strlen(tmp));
+            printf("command.argv[%d] : %s\n", command.argc, command.argv[command.argc]);
             command.argc++;
             memset(tmp, 0, len_cmd);
             continue;
@@ -59,7 +134,11 @@ void parser_cmd(char *cmd)
         {
             if (i == len_cmd - 1)
             {
+                char c = cmd[i];
+                strncat(tmp, &c, 1);
+                printf("tmp : %s\n", tmp);
                 strncpy(command.argv[command.argc], tmp, strlen(tmp));
+                printf("command.argv[%d] : %s\n", command.argc, command.argv[command.argc]);
                 command.argc++;
                 break;
             }
@@ -67,25 +146,5 @@ void parser_cmd(char *cmd)
             strncat(tmp, &c, 1);
             // printf("tmp : %s\n", tmp);
         }
-    }
-    for (int i = 0; i < command.argc; i++)
-    {
-        printf("aaa %s\n", command.argv[i]);
-    }
-}
-
-int main()
-{
-
-    while (1)
-    {
-        char cmd[0x100];
-        memset(cmd, 0, 0x100);
-
-        printf("\n$ ");
-        fflush(stdout);
-        read(0, cmd, 100);
-        cmd[strlen(cmd) - 1] = '\x00'; // ' ls -lh -a \n'
-        parser_cmd(cmd);
     }
 }
--- test.c
+++ test.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-void strip1(char *a)
+
+
+void hello()
 {
-    strcpy(a, "123");
+    printf("hello func");
 }
 
-void strip2(char **a)
-{
-    strcpy(*a, "123");
-}
 int main()
 {
-    char a[10] = "abc";
-    strip1(a);
-    printf("a : %s\n", a);
-    char *b = a;
-    strip2(&b);
-    printf("a : %s\n", a);
+    
 }
