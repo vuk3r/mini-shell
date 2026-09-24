0be8d7892096a243be114181212c90bbca1f0afa
Ld4vicl <connguathanhtroia@gmail.com>
2026-09-24 21:26:37 +0700

    test

--- test.c
+++ test.c
@@ -6,12 +6,10 @@
 int main()
 {
     int fd = open("./abc", O_RDONLY);
-
     if (fd == -1)
     {
         fprintf(stderr, "Không mở được '%s': %s\n",
                 "./abc", strerror(errno));
         perror("error");
-        return -1;
     }
 }
